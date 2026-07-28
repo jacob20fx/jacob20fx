e6ebf6ecc0e82fad560c3f21eef1ee8e71ca4b20
jacob20fx <szeerszen1234@gmail.com>
2026-07-28 19:09:57 +0200

    Update Supabase configuration values
    dodano konfiguracje Supabase

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 window.J20_CONFIG = {
-  SUPABASE_URL: "WKLEJ_TUTAJ_SUPABASE_URL",
-  SUPABASE_ANON_KEY: "WKLEJ_TUTAJ_SUPABASE_ANON_KEY",
+  SUPABASE_URL: "https://fhofuzvxxthmigjganlp.supabase.co",
+  SUPABASE_ANON_KEY: "sb_publishable__u7b139MjT9yz4w5nmPDng_tG6qdvXo",
   TELEGRAM_URL: "https://t.me/jacob20FX"
 };
